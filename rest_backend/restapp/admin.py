3a4e5429c311973d38b2a3e13f5068190069e1f5
Shubham Goel <ass@gmail.com>
2024-12-01 21:32:52 +0530

    sensitive code change

--- rest_backend/restapp/admin.py
+++ rest_backend/restapp/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Company)
-admin.site.register(Order)
-admin.site.register(Product)
-admin.site.register(Profile)
-admin.site.register(Address)
-admin.site.register(Image)
+# admin.site.register(Company)
+# admin.site.register(Order)
+# admin.site.register(Product)
+# admin.site.register(Profile)
+# admin.site.register(Address)
+# admin.site.register(Image)
